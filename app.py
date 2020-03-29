f5d219dac94962a6db04cab193d86ef73616cec9
Poranut Chollavorn <pipech@gmail.com>
2020-03-29 11:35:33 +0700

    feat(app): add google analytics tracking

--- app.py
+++ app.py
@@ -26,6 +26,35 @@ app = dash.Dash(
 )
 app.title = title
 app.layout = layout
+app.index_string = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-162149795-1"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-162149795-1');
+        </script>
+
+        {%metas%}
+        <title>{%title%}</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>"""
+
 
 # for aws elasticbeanstalk
 application = app.server
